4128a8df6cf5875261da3d4d32fcffc5ab80c3fe
Ralf S. Engelschall <rse@engelschall.com>
2015-05-23 18:46:02 +0200

    use a more specific platform matching to support the new Prince 10 (where no generic Linux binaries exist anymore)

--- prince-npm.js
+++ prince-npm.js
@@ -73,27 +73,74 @@ var princeInfo = function () {
 
 /*  return download URL for latest PrinceXML distribution  */
 var princeDownloadURL = function () {
-    var vendor = {
-        version: "9.0r5",
-        download: {
-            "ia32-win32":   "http://www.princexml.com/download/prince-@V@-setup.exe",
-            "x64-win32":    "http://www.princexml.com/download/prince-@V@-setup.exe",
-            "ia32-freebsd": "http://www.princexml.com/download/prince-@V@-freebsd82-static.tar.gz",
-            "x64-freebsd":  "http://www.princexml.com/download/prince-@V@-freebsd82-static.tar.gz",
-            "ia32-linux":   "http://www.princexml.com/download/prince-@V@-linux-static.tar.gz",
-            "x64-linux":    "http://www.princexml.com/download/prince-@V@-linux-amd64-static.tar.gz",
-            "ia32-sunos":   "http://www.princexml.com/download/prince-@V@-sol10x86-opencsw.tar.gz",
-            "x64-sunos":    "http://www.princexml.com/download/prince-@V@-sol10x86-opencsw.tar.gz",
-            "ia32-darwin":  "http://www.princexml.com/download/prince-@V@-macosx.tar.gz",
-            "x64-darwin":   "http://www.princexml.com/download/prince-@V@-macosx.tar.gz"
+    return new promise(function (resolve /*, reject */) {
+        var id = process.arch + "-" + process.platform;
+        if (id.match(/^(?:ia32|x64)-win32$/))
+            resolve("http://www.princexml.com/download/prince-10-setup.exe");
+        else if (id.match(/^(?:ia32|x64)-darwin/))
+            resolve("http://www.princexml.com/download/prince-10-macosx.tar.gz");
+        else {
+            child_process.exec("sh " + __dirname + "/shtool platform -t binary", function (error, stdout /*, stderr */) {
+                if (error) {
+                    console.log("ERROR: failed to determine platform details on platform \"" + id + "\"");
+                    process.exit(1);
+                }
+                var platform = stdout.toString().replace(/^(\S+).*$/, "$1");
+                if (id.match(/^(?:ia32|x64)-linux/)) {
+                    if (platform.match(/^ix86-ubuntu1[01](?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-ubuntu10.04-amd64.tar.gz");
+                    else if (platform.match(/^amd64-ubuntu1[01](?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-ubuntu10.04-i386.tar.gz");
+                    else if (platform.match(/^ix86-ubuntu1[23](?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-ubuntu12.04-amd64.tar.gz");
+                    else if (platform.match(/^amd64-ubuntu1[23](?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-ubuntu12.04-i386.tar.gz");
+                    else if (platform.match(/^ix86-ubuntu1[45](?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-ubuntu14.04-amd64.tar.gz");
+                    else if (platform.match(/^amd64-ubuntu1[45](?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-ubuntu14.04-i386.tar.gz");
+                    else if (platform.match(/^amd64-debian8(?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-debian8.0-amd64.tar.gz");
+                    else if (platform.match(/^amd64-debian7(?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-debian7.4-amd64.tar.gz");
+                    else if (platform.match(/^amd64-centos7(?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-centos7-x86_64.tar.gz");
+                    else if (platform.match(/^amd64-centos6(?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-centos6-x86_64.tar.gz");
+                    else if (platform.match(/^ix86-centos6(?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-centos6-i386.tar.gz");
+                    else if (platform.match(/^amd64-centos5(?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-centos5-x86_64.tar.gz");
+                    else if (platform.match(/^ix86-centos5(?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-centos5-i386.tar.gz");
+                    else if (platform.match(/^amd64-suse13(?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-opensuse13.2-x86_64.tar.gz");
+                    else if (platform.match(/^amd64-suse1[12](?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-opensuse11-x86_64.tar.gz");
+                    else if (platform.match(/^ix86-suse1[12](?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-opensuse11-i586.tar.gz");
+                    else if (id.match(/^ia32-/))
+                        resolve("http://www.princexml.com/download/prince-9.0r5-linux-static.tar.gz");
+                    else if (id.match(/^x64-/))
+                        resolve("http://www.princexml.com/download/prince-9.0r5-linux-amd64-static.tar.gz");
+                }
+                else if (id.match(/^ia32-freebsd/))
+                    resolve("http://www.princexml.com/download/prince-10r2-freebsd10.1-i386-static.tar.gz");
+                else if (id.match(/^x64-freebsd/))
+                    resolve("http://www.princexml.com/download/prince-10r2-freebsd10.1-amd64-static.tar.gz");
+                else if (id.match(/^(?:ia32|x64)-sunos/)) {
+                    if (platform.match(/11(?:\.\d+)*$/))
+                        resolve("http://www.princexml.com/download/prince-10-sol11x86.tar.gz");
+                    else
+                        resolve("http://www.princexml.com/download/prince-9.0r5-sol10x86-opencsw.tar.gz");
+                }
+                else {
+                    console.log("ERROR: PrinceXML not available for platform \"" + platform + "\"");
+                    process.exit(1);
+                }
+            });
         }
-    };
-    var id = process.arch + "-" + process.platform;
-    if (typeof vendor.download[id] === "undefined") {
-        console.log("ERROR: PrinceXML not available for platform \"" + id + "\"");
-        process.exit(1);
-    }
-    return vendor.download[id].replace(/@V@/g, vendor.version);
+    });
 };
 
 /*  download data from URL  */
@@ -179,43 +226,44 @@ if (process.argv[2] === "install") {
         console.log("-- found prince(1) version: " + chalk.blue(prince.version));
     }, function (/* error */) {
         console.log("++ downloading PrinceXML distribution");
-        var url = princeDownloadURL();
-        downloadData(url).then(function (data) {
-            console.log("++ locally unpacking PrinceXML distribution");
-            destdir = path.join(__dirname, "prince");
-            var destfile;
-            if (process.platform === "win32") {
-                destfile = path.join(__dirname, "prince.exe");
-                fs.writeFileSync(destfile, data, { encoding: null });
-                var args = [ "/s", "/a", "/vTARGETDIR=\"" + path.resolve(destdir) + "\" /qn" ];
-                child_process.execFile(destfile, args, function (error, stdout, stderr) {
-                    if (error !== null) {
-                        console.log(chalk.red("** ERROR: failed to extract: " + error));
-                        stdout = stdout.toString();
-                        stderr = stderr.toString();
-                        if (stdout !== "")
-                            console.log("** STDOUT: " + stdout);
-                        if (stderr !== "")
-                            console.log("** STDERR: " + stderr);
-                    }
-                    else {
+        princeDownloadURL().then(function (url) {
+            downloadData(url).then(function (data) {
+                console.log("++ locally unpacking PrinceXML distribution");
+                destdir = path.join(__dirname, "prince");
+                var destfile;
+                if (process.platform === "win32") {
+                    destfile = path.join(__dirname, "prince.exe");
+                    fs.writeFileSync(destfile, data, { encoding: null });
+                    var args = [ "/s", "/a", "/vTARGETDIR=\"" + path.resolve(destdir) + "\" /qn" ];
+                    child_process.execFile(destfile, args, function (error, stdout, stderr) {
+                        if (error !== null) {
+                            console.log(chalk.red("** ERROR: failed to extract: " + error));
+                            stdout = stdout.toString();
+                            stderr = stderr.toString();
+                            if (stdout !== "")
+                                console.log("** STDOUT: " + stdout);
+                            if (stderr !== "")
+                                console.log("** STDERR: " + stderr);
+                        }
+                        else {
+                            fs.unlinkSync(destfile);
+                            console.log("-- OK: local PrinceXML installation now available");
+                        }
+                    });
+                }
+                else {
+                    destfile = path.join(__dirname, "prince.tgz");
+                    fs.writeFileSync(destfile, data, { encoding: null });
+                    extractTarball(destfile, destdir, 1).then(function () {
                         fs.unlinkSync(destfile);
                         console.log("-- OK: local PrinceXML installation now available");
-                    }
-                });
-            }
-            else {
-                destfile = path.join(__dirname, "prince.tgz");
-                fs.writeFileSync(destfile, data, { encoding: null });
-                extractTarball(destfile, destdir, 1).then(function () {
-                    fs.unlinkSync(destfile);
-                    console.log("-- OK: local PrinceXML installation now available");
-                }, function (error) {
-                    console.log(chalk.red("** ERROR: failed to extract: " + error));
-                });
-            }
-        }, function (error) {
-            console.log(chalk.red("** ERROR: failed to download: " + error));
+                    }, function (error) {
+                        console.log(chalk.red("** ERROR: failed to extract: " + error));
+                    });
+                }
+            }, function (error) {
+                console.log(chalk.red("** ERROR: failed to download: " + error));
+            });
         });
     });
 }
